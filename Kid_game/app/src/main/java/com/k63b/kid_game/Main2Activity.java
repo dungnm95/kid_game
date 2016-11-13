f877d8f3a1c69eb5d9ba1b81349840daee0318f7
DungNM <ech.op411@gmail.com>
2016-11-13 23:42:06 +0700

    tạo màn hình nhận biết số

--- Kid_game/app/src/main/java/com/k63b/kid_game/Main2Activity.java
+++ Kid_game/app/src/main/java/com/k63b/kid_game/Main2Activity.java
@@ -1,13 +1,24 @@
 package com.k63b.kid_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    ImageView back;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        back = (ImageView)findViewById(R.id.btn_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent screen_main = new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(screen_main);
+            }
+        });
     }
 }
--- Kid_game/app/src/main/java/com/k63b/kid_game/Main3Activity.java
+++ Kid_game/app/src/main/java/com/k63b/kid_game/Main3Activity.java
@@ -1,13 +1,24 @@
 package com.k63b.kid_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Main3Activity extends AppCompatActivity {
-
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        back = (ImageView)findViewById(R.id.btn_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent screen_main = new Intent(Main3Activity.this,MainActivity.class);
+                startActivity(screen_main);
+            }
+        });
     }
 }
